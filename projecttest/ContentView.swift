b445a3a4a7dae86fbebd7c44197ceb05d8bbbabd
Nick Melekian <nick.melekian@gmail.com>
2024-12-10 09:37:37 -0500

    Initial Commit

---
+++ projecttest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projecttest
+//
+//  Created by Nick Melekian on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projecttest/projecttestApp.swift
@@ -0,0 +1,17 @@
+//
+//  projecttestApp.swift
+//  projecttest
+//
+//  Created by Nick Melekian on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct projecttestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
